96a8de76569c1604d9a3338ead8f6ba5adeb48f7
Chris Sewell <chrisj_sewell@hotmail.com>
2019-10-17 00:19:51 +0100

    grouppaths: initial addition to aiida-core

---
+++ aiida/tools/groups/__init__.py
@@ -0,0 +1,14 @@
+# This file is part of the AiiDA code.                                    #
+#                                                                         #
+# The code is hosted on GitHub at https://github.com/aiidateam/aiida-core #
+# For further information on the license, see the LICENSE.txt file        #
+# For further information please visit http://www.aiida.net               #
+###########################################################################
+# pylint: disable=wildcard-import,undefined-variable
+"""Provides tools for interacting with AiiDA Groups."""
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+from .grouppaths import *
+
+__all__ = (grouppaths.__all__,)
